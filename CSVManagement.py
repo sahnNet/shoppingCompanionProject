fa45f61ce61203fe4f100c48acec6e251e5c366f
SAHN net <sahnnet.working@gmail.com>
2022-04-29 19:14:12 +0430

    Script modification

--- CSVManagement.py
+++ CSVManagement.py
@@ -6,16 +6,15 @@ def update_file_csv(path_in: str, path_to: str):
     op = open(path_in, "r")
     dt = csv.DictReader(op)
     up_dt = []
+
     for r in dt:
-        print(r)
         row = {'intent': r['\ufeffintent'], 'value': r['value']}
         up_dt.append(row)
-    print(up_dt)
+
     op.close()
     op = open(path_to, "a", newline='')
     headers = ['intent', 'value']
     data = csv.DictWriter(op, delimiter=',', fieldnames=headers)
-    data.writerow(dict((heads, heads) for heads in headers))
     data.writerows(up_dt)
 
     op.close()
@@ -23,6 +22,6 @@ def update_file_csv(path_in: str, path_to: str):
 
 # Only when this is the original executable file is the condition true
 if __name__ == '__main__':
-    # update_file_csv('ancillary.csv', 'answers.csv')
-    update_file_csv('ancillary.csv', 'questions.csv')
+    update_file_csv('ancillary.csv', 'answers.csv')
+    # update_file_csv('ancillary.csv', 'questions.csv')
     # pass
