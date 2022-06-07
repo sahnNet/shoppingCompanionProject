a9a5e21a1bb4b05c249b7fa48503942e5f3ac482
moh-b <m.b.mohammadbayati@gmail.com>
2022-06-07 20:33:15 +0430

    Fixed a problem where csv file didn't transfer properly

--- CSVManagement.py
+++ CSVManagement.py
@@ -3,16 +3,16 @@ import csv
 
 # Updating csv files
 def update_file_csv(path_in: str, path_to: str):
-    op = open(path_in, "r")
+    op = open(path_in, "r", encoding='utf-8')
     dt = csv.DictReader(op)
     up_dt = []
 
     for r in dt:
-        row = {'intent': r['\ufeffintent'], 'value': r['value']}
+        row = {'intent': r['intent'], 'value': r['value']}
         up_dt.append(row)
 
     op.close()
-    op = open(path_to, "a", newline='')
+    op = open(path_to, "a", newline='', encoding='utf-8')
     headers = ['intent', 'value']
     data = csv.DictWriter(op, delimiter=',', fieldnames=headers)
     data.writerows(up_dt)
